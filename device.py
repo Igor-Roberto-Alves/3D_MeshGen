9f66035b6bed25f2447b28849750d552499c63bf
Igor-Roberto-Alves <al.igor.alves@impatech.edu.br>
2026-05-31 14:40:10 -0300

    For see device with terminal

--- device.py
+++ device.py
@@ -4,4 +4,10 @@ device = torch.device(
     "cuda" if torch.cuda.is_available() 
     else "cpu"
 )
-print(f"Using device: {device}")
+
+
+
+if device == torch.device("cuda"):
+    print(f"😁😁😎 Using device: {device}")
+else:
+    print(f"😡😡🫨  Using device: {device}")
