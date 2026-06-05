decd1d22295dc685c8535bd8c1638b48bc450a03
al.igor.alves <al.igor.alves@impatech.org.br>
2026-06-05 16:39:09 -0300

    break param

--- params.py
+++ params.py
@@ -1,7 +1,7 @@
 import torch
-from src.Vae import DualBranchPointVAE
+from src.Vae import Vae
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = DualBranchPointVAE().to(device)
+    model = Vae().to(device)
     model.report_parameters()
