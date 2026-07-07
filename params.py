abd498329895b4fc9150f61f1f4aee76601538be
Igor-Roberto-Alves <al.igor.alves@impatech.edu.br>
2026-07-07 18:57:06 -0300

    cleaning

--- params.py
+++
@@ -1,7 +0,0 @@
-import torch
-from src.Vae import Vae
-
-if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = Vae().to(device)
-    model.report_parameters()
